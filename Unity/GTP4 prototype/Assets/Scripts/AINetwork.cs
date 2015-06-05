4b39937394adfbce160604561e448cec5d37e960
Stan <stan.vandenbroek@student.fontys.nl>
2015-06-05 12:30:25 +0200

    added AI syncing over network

---
+++ Unity/GTP4 prototype/Assets/Scripts/AINetwork.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AINetwork : Photon.MonoBehaviour  
+{
+    private AISphere AIScript;
+
+    void Awake()
+    {
+        this.AIScript = GetComponentInChildren<AISphere>();
+
+        if (photonView.isMine)
+        {
+            //MINE: local player, simply enable the local scripts
+            this.AIScript.enabled = true;
+        }
+        else
+        {
+            this.AIScript.enabled = false;
+        }
+
+        gameObject.name = gameObject.name + photonView.viewID;
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            //We own this player: send the others our data
+            stream.SendNext(transform.position);
+        }
+        else
+        {
+            //Network player, receive data
+            correctPlayerPos = (Vector3)stream.ReceiveNext();
+        }
+    }
+
+    private Vector3 correctPlayerPos = Vector3.zero; //We lerp towards this
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!photonView.isMine)
+        {
+            //Update remote player (smooth this, this looks good, at the cost of some accuracy)
+            transform.position = Vector3.Lerp(transform.position, correctPlayerPos, Time.deltaTime * 5);
+        }
+    }
+}
