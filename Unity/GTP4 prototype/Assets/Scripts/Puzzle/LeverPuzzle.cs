2180c9776fe5a5b211e6aab051ced03f378ff0d4
Phyxel <cypher@live.nl>
2015-07-04 21:58:32 +0200

    LeverPuzzle disabled

--- Unity/GTP4 prototype/Assets/Scripts/Puzzle/LeverPuzzle.cs
+++ Unity/GTP4 prototype/Assets/Scripts/Puzzle/LeverPuzzle.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LeverPuzzle : MonoBehaviour 
+public class LeverPuzzle : MonoBehaviour
 {
     public AudioClip clickSound;
     public AudioClip finishedSound;
@@ -12,24 +12,24 @@ public class LeverPuzzle : MonoBehaviour
     private bool prevLever1 = false;
 
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
-    {
-	    if(lever1.leverActivated != prevLever1)
-        {
-            AudioSource.PlayClipAtPoint(this.clickSound, this.transform.position);
-        }
 
-        if (lever1.leverActivated)
-        {
-            AudioSource.PlayClipAtPoint(this.finishedSound, this.transform.position);
-            this.gameObject.SetActive(false);
-        }
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (lever1.leverActivated != prevLever1)
+        //{
+        //    AudioSource.PlayClipAtPoint(this.clickSound, this.transform.position);
+        //}
+
+        //if (lever1.leverActivated)
+        //{
+        //    AudioSource.PlayClipAtPoint(this.finishedSound, this.transform.position);
+        //    this.gameObject.SetActive(false);
+        //}
+    }
 }
